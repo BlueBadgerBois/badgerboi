51dec5ad407bb15fc2821371bd5a566a1626e0f3
jboretsky <jonah.boretsky@gmail.com>
2018-01-19 14:54:17 -0800

    add basic web handler for add

--- web/main.go
+++ web/main.go
@@ -1,29 +1,47 @@
 package main
 
 import (
-	"html/template"
-	"github.com/kabukky/httpscerts" //might download later for faster import OR implement own version of cert generation
-	"net/http"
-	"log"
+  "html/template"
+  "github.com/kabukky/httpscerts" //might download later for faster import OR implement own version of cert generation
+  "net/http"
+  "log"
+  "fmt"
 )
 
 func handler(w http.ResponseWriter, r *http.Request){
-	temp, err := template.ParseFiles("templates/login.html")
-	if err != nil {
-		log.Println("template parsing error: ", err)
-	}
+  temp, err := template.ParseFiles("templates/login.html")
+  if err != nil {
+    log.Println("template parsing error: ", err)
+  }
 
-	p := "hey" //filler data so function Execute executes
-	err = temp.Execute(w, p)
-	if err != nil{
-		log.Println("template execution error: ", err)
-	}
+  p := "hey" //filler data so function Execute executes
+  err = temp.Execute(w, p)
+  if err != nil{
+    log.Println("template execution error: ", err)
+  }
 
-	log.Println("Login Page rendered.")
+  log.Println("Login Page rendered.")
+}
+
+func addHandler(w http.ResponseWriter, r *http.Request) {
+  if r.Method == "POST" {
+    type addJson struct {
+      UID string
+      Amount string
+    }
+    r.ParseForm()
+    addReq := addJson{
+      UID: r.Form.Get("uID"),
+      Amount: r.Form.Get("amount"),
+    }
+    fmt.Fprintf(w,
+      "User ID: " + addReq.UID + "\n" +
+      "Amount Added: " + addReq.Amount)
+  }
 }
 
 func main() {
-	// Check if the cert files are available.
+  // Check if the cert files are available.
   err := httpscerts.Check("cert.pem", "key.pem")
   // If they are not available, generate new ones.
   if err != nil {
@@ -33,8 +51,7 @@ func main() {
     }
   }
 
-	http.HandleFunc("/", handler)
-	http.ListenAndServeTLS(":8081", "cert.pem", "key.pem", nil)
-	
-	//redirect http to https here
+  http.HandleFunc("/", handler)
+  http.HandleFunc("/add", addHandler)
+  http.ListenAndServeTLS(":8081", "cert.pem", "key.pem", nil)
 }
