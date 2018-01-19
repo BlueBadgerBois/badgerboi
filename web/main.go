38638280e51cd8226710e0e9c13e50c4a57d32a7
Edgardo Cuello <ecuellog@gmail.com>
2018-01-18 18:32:04 -0800

    Basic server for web-component

--- web/main.go
+++ web/main.go
@@ -1,10 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 const db_host = "cassandra"
 const db_keyspace = "badgerboi"
 
+func handler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, " Hello %s", r.URL.Path[1:])
+}
+
 func main() {
 	// time.Sleep(20 * time.Second) // try sleep until cassandra is up
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe("0.0.0.0:8080", nil)
 
 	// connect to db
 	db := DB{};
